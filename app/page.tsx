b23277a71d34f542b431aaaa1ff0595e0356f50c
DemonicFlame <arkaprovo007@gmail.com>
2024-09-25 00:42:35 +0530

    commit 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { categories } from "@/constants";
+import fetchNews from "@/lib/fetchNews";
 
-export default function Homepage() {
+async function Homepage() {
+  //fetch news data
+  const news: NewsResponse = await fetchNews(categories.join(","));
   return <div>Homepage</div>;
 }
+
+export default Homepage;
---
+++ lib/fetchNews.ts
@@ -0,0 +1,10 @@
+const fetchNews = async (
+  category?: Category | string,
+  keywords?: string,
+  isDynamic?: boolean
+) => {
+  //GraphQL query
+  //sort function for images present vs not present
+};
+
+export default fetchNews;
